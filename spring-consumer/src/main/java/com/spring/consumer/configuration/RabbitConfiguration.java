bb631d6918165a24cf4ddc0f97a0e5c95a4d3a88
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2022-01-03 19:37:39 -0300

    Configuração do RabbitMQ na API Consumer

--- spring-consumer/src/main/java/com/spring/consumer/configuration/RabbitConfiguration.java
+++ spring-consumer/src/main/java/com/spring/consumer/configuration/RabbitConfiguration.java
@@ -2,15 +2,29 @@ package com.spring.consumer.configuration;
 
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+/*
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
+*/
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class RabbitConfiguration {
 
+	/*
 	@Bean
 	public MessageConverter jsonConvert() {
 		return new Jackson2JsonMessageConverter();
@@ -23,5 +37,35 @@ public class RabbitConfiguration {
 		configurer.configure(factory, connectionFactory);
 		factory.setMessageConverter(jsonConvert());
 		return factory;
+	}
+	
+	*/
+	
+	@Autowired
+	private ConnectionFactory connectionFactory;
+	
+	@Bean
+	SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory() {
+		SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+		factory.setConnectionFactory(connectionFactory);
+		factory.setMessageConverter(jacksonConverter());
+		return factory;
+	}
+	
+	@Bean
+	RabbitTemplate rabbitTemplate() {
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(jacksonConverter());
+		return rabbitTemplate;
+	}
+	
+	@Bean
+	Jackson2JsonMessageConverter jacksonConverter() {
+		final ObjectMapper mapper = Jackson2ObjectMapperBuilder.json()
+									.modules(new JavaTimeModule())
+									.dateFormat(new StdDateFormat())
+									.build();
+		return new Jackson2JsonMessageConverter(mapper);
+		
 	}
 }
