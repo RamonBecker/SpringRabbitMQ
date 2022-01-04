5e9c247c786d47aa80ac60ffe16b612a685d56b8
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2022-01-04 19:27:16 -0300

    Testando o Reprocessamento

--- spring-consumer/src/main/java/com/spring/consumer/SpringConsumerApplication.java
+++ spring-consumer/src/main/java/com/spring/consumer/SpringConsumerApplication.java
@@ -4,9 +4,11 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @EnableRabbit
+@EnableScheduling
 @SpringBootApplication
 @ComponentScan("com.spring.consumer")
 public class SpringConsumerApplication {
